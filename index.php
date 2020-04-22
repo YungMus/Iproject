0f8335e433cd8228decac9ed68c7a833f13a4adf
YungMus <64072245+YungMus@users.noreply.github.com>
2020-04-22 11:42:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,5 @@ require_once("includes/header.php");
 
 <?php
 require_once("includes/foundation_script.php");
-<<<<<<< HEAD
 require_once("includes/footer.php");
-=======
-include_once("includes/footer.html");
->>>>>>> 48d1cd8bb246320e60fbc3671564677e988e399d
 ?>
