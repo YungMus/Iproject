07c16ad4d4db0fec7c903095487f48f289b9ebfe
Dragon4Ever <64072048+Dragon4Ever@users.noreply.github.com>
2020-04-23 18:21:15 +0200

    Image on index.php

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ $title = 'Homepage';
 
 require_once("includes/header.php");
 ?>
+<div class="hero-section">
+    <div class="hero-section-text">
+        <h1>Header</h1>
+        <h5>subtitle about anything you like</h5>
+    </div>
+</div>
 
 <?php
 require_once("includes/foundation_script.php");
