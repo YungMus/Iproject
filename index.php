7ebfbe9f9623c688aaba6d7e126e1fe8053b1df7
Dragon4Ever <64072048+Dragon4Ever@users.noreply.github.com>
2020-04-23 20:35:15 +0200

    index.php changes

--- index.php
+++ index.php
@@ -9,6 +9,50 @@ require_once("includes/header.php");
         <h5>subtitle about anything you like</h5>
     </div>
 </div>
+<br>
+<nav class="hover-underline-menu" data-menu-underline-from-center>
+    <ul class="menu align-center">
+        <li><a href="#">One</a></li>
+        <li><a href="#">Two</a></li>
+        <li><a href="#">Three</a></li>
+        <li><a href="#">Four</a></li>
+    </ul>
+</nav>
+<br>
+<div class="product-card">
+    <div class="product-card-thumbnail">
+        <a href="#"><img src="https://placehold.it/180x180"/></a>
+    </div>
+    <h2 class="product-card-title"><a href="#">Product Name</a></h2>
+    <span class="product-card-desc">Product Description</span>
+    <span class="product-card-price">$9.99</span><span class="product-card-sale">$12.99</span>
+    <div class="product-card-colors">
+        <button href="#" class="product-card-color-option"><img src="https://placehold.it/30x30"/></button>
+        <button href="#" class="product-card-color-option"><img src="https://placehold.it/30x30"/></button>
+        <button href="#" class="product-card-color-option"><img src="https://placehold.it/30x30"/></button>
+        <button href="#" class="product-card-color-option"><img src="https://placehold.it/30x30"/></button>
+    </div>
+</div>
+<br>
+<div class="product-card">
+    <div class="product-card-thumbnail">
+        <a href="#"><img src="https://placehold.it/180x180"/></a>
+    </div>
+    <h2 class="product-card-title"><a href="#">Product Name</a></h2>
+    <span class="product-card-desc">Product Description</span>
+    <span class="product-card-price">$9.99</span><span class="product-card-sale">$12.99</span>
+    <div class="product-card-colors">
+        <button href="#" class="product-card-color-option"><img src="https://placehold.it/30x30"/></button>
+        <button href="#" class="product-card-color-option"><img src="https://placehold.it/30x30"/></button>
+        <button href="#" class="product-card-color-option"><img src="https://placehold.it/30x30"/></button>
+        <button href="#" class="product-card-color-option"><img src="https://placehold.it/30x30"/></button>
+    </div>
+</div>
+
+
+
+
+
 
 <?php
 require_once("includes/foundation_script.php");
