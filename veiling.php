24292990dd15971a311e4aae158596c6a0c4f08c
YungMus <64072245+YungMus@users.noreply.github.com>
2020-04-23 15:30:47 +0200

    veiling.php

--- veiling.php
+++ veiling.php
@@ -1,9 +1,17 @@
 <?php
 $title = 'Veiling';
 require_once("includes/header.php");
-?>
 
-<?php
+$html = '<div class="grid-x grid-padding-x">
+  <div class="cell small-4 align-self-top">Align top. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non harum laborum cum voluptate vel, eius adipisci similique dignissimos nobis at excepturi incidunt fugit molestiae quaerat, consequuntur porro temporibus. Nisi, ex?Align top. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non harum laborum cum voluptate vel, eius adipisci similique dignissimos nobis at excepturi incidunt fugit molestiae quaerat, consequuntur porro temporibus. Nisi, ex?Align top. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non harum laborum cum voluptate vel, eius adipisci similique dignissimos nobis at excepturi incidunt fugit molestiae quaerat, consequuntur porro temporibus. Nisi, ex?</div>
+  <div class="cell small-4 flex-container flex-dir-column">
+    <div class="callout primary flex-child-auto-veiling">Veiling</div>
+    <div class="callout primary flex-child-auto-veiling">Veiling</div>
+  </div>
+</div>';
+
+echo $html;
+
 require_once("includes/foundation_script.php");
 require_once("includes/footer.php");
 ?>
