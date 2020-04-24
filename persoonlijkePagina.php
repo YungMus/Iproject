a3ad45dc1ffb415f3dd5a51a8cf34c5e11b40b60
NoaEijkholt <NGA.Eijkholt@student.han.nl>
2020-04-24 09:40:16 +0200

    Persoonlijke pagina en Voorwaarden

--- persoonlijkePagina.php
+++ persoonlijkePagina.php
@@ -4,6 +4,45 @@ require_once("includes/header.php");
 ?>
 
 <?php
+    $html = '<div class="row flex-container">
+    <div class="product-card-thumbnail padding-r">
+        <a href="#"><img src="https://placehold.it/180x180"/></a>
+    </div>
+    <div>
+    <div class="callout text-center align-center-middle">
+        <h1>iConcepts</h1>
+    </div>
+</div>
+</div>
+
+<div class="row flex-container align-left">
+    <div class="callout text-center align-left padding-r">
+        <p>menu items</p>
+    </div>
+    <div class="callout text-center align-right">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum a libero hendrerit, laoreet eros vitae, accumsan eros.
+                Donec dui urna, tempus non elit a, tincidunt pulvinar lectus.
+                Maecenas non risus auctor tellus convallis volutpat.
+                Mauris aliquam feugiat tortor id blandit. Donec laoreet lobortis turpis vitae semper.
+                Quisque gravida ornare euismod.</p>
+    </div>
+</div>
+
+    <div class="row flex-container align-center">
+        <div class="callout text-center">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum a libero hendrerit, laoreet eros vitae, accumsan eros.
+                Donec dui urna, tempus non elit a, tincidunt pulvinar lectus.
+                Maecenas non risus auctor tellus convallis volutpat.
+                Mauris aliquam feugiat tortor id blandit. Donec laoreet lobortis turpis vitae semper.
+                Quisque gravida ornare euismod.</p>
+        </div>
+    </div>';
+    echo $html;
+?>
+
+<?php
 require_once("includes/foundation_script.php");
 require_once("includes/footer.php");
 ?>
--- voorwaardenCondities.php
+++ voorwaardenCondities.php
@@ -5,11 +5,21 @@ require_once("includes/header.php");
 
 <?php
 $html = '<div class="flex-container align-spaced">
-            <div class="navigatiebalk">
-                <p>lorem ipsum</p>
+            <div class="navigatiebalk callout">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum a libero hendrerit, laoreet eros vitae, accumsan eros.
+                Donec dui urna, tempus non elit a, tincidunt pulvinar lectus.
+                Maecenas non risus auctor tellus convallis volutpat.
+                Mauris aliquam feugiat tortor id blandit. Donec laoreet lobortis turpis vitae semper.
+                Quisque gravida ornare euismod.</p>
             </div>
-            <div class="voorwaarden">
-                <p>lorem ipsum 2</p>
+            <div class="voorwaarden callout align-justify">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum a libero hendrerit, laoreet eros vitae, accumsan eros.
+                Donec dui urna, tempus non elit a, tincidunt pulvinar lectus.
+                Maecenas non risus auctor tellus convallis volutpat.
+                Mauris aliquam feugiat tortor id blandit. Donec laoreet lobortis turpis vitae semper.
+                Quisque gravida ornare euismod.</p>
             </div>
 </div>';
 
