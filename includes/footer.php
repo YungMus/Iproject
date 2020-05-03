308d92349e2b0951380f511d887f2f10e113c05b
Dragon4Ever <64072048+Dragon4Ever@users.noreply.github.com>
2020-05-03 13:28:11 +0200

    href verbetert

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <?php
 $html = '<footer>
-    <a href="voorwaarden.php">Klik hier voor de voorwaarden</a> 
+    <a href="voorwaardenCondities.php">Klik hier voor de voorwaarden</a> 
     <p>&copy; 2020 groep 43 1E </p>
 </footer>
 </body>
