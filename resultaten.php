077554c62c898cf79de4f79d3360b764d5304aef
YungMus <64072245+YungMus@users.noreply.github.com>
2020-05-08 12:35:45 +0200

    resultaten searchbar

---
+++ resultaten.php
@@ -0,0 +1,35 @@
+<?php
+$title = 'Inlogpagina';
+$link = 'veilingCategorieOverzicht.php';
+
+require_once("includes/header.php");
+require_once("resultaten.inc.php");
+
+?>
+
+    <div class="grid-x grid-padding-y grid-padding-x">
+        <div class="cell small-4 align-self-top"</div>
+
+        <div class="cell small-4 flex-container flex-dir-column">
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-shrink">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+            <div class="callout primary flex-child-auto">Veiling</div>
+        </div>
+    </div>
+
+<?php
+require_once("includes/foundation_script.php");
+require_once("includes/footer.php");
+?>
