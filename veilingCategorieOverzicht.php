90f181102cb8afbb127cd494eb84477e041d1002
Dragon4Ever <64072048+Dragon4Ever@users.noreply.github.com>
2020-05-10 22:33:08 +0200

    rubriekenboom basis neer gezet

--- veilingCategorieOverzicht.php
+++ veilingCategorieOverzicht.php
@@ -6,15 +6,68 @@ require_once("includes/header.php");
 ?>
 
 <div class="grid-x grid-padding-y grid-padding-x">
-  <div class="cell small-4 align-self-top">Align top. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-  Non harum laborum cum voluptate vel, eius adipisci similique dignissimos nobis at excepturi incidunt fugit molestiae quaerat, 
-  consequuntur porro temporibus. Nisi, ex?Align top. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-  Non harum laborum cum voluptate vel, eius adipisci similique dignissimos nobis at excepturi incidunt fugit molestiae quaerat, 
-  consequuntur porro temporibus. Nisi, ex?Align top. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-  Non harum laborum cum voluptate vel, eius adipisci similique dignissimos nobis at excepturi incidunt fugit molestiae quaerat, 
-  consequuntur porro temporibus. Nisi, ex?</div>
-  
-  <div class="cell small-4 flex-container flex-dir-column">
+    <ul class="multilevel-accordion-menu vertical menu" data-accordion-menu>
+        <li>
+            <a href="#">Item 1</a>
+            <ul class="menu vertical sublevel-1">
+                <li>
+                    <a href="#">Sub-item 1</a>
+                    <ul class="menu vertical sublevel-2">
+                        <li><a class="subitem" href="#">Thing 1</a></li>
+                        <li><a class="subitem" href="#">Thing 2</a></li>
+                        <li><a class="subitem" href="#">Thing 3</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Sub-item 2</a>
+                    <ul class="menu vertical sublevel-2">
+                        <li>
+                            <a href="#">Super-sub-item 1</a>
+                            <ul class="menu vertical sublevel-3">
+                                <li><a class="subitem" href="#">Thing 1</a></li>
+                                <li><a class="subitem" href="#">Thing 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a class="subitem" href="#">Thing 2</a></li>
+                    </ul>
+                </li>
+                <li><a class="subitem" href="#">Thing 1</a></li>
+                <li><a class="subitem" href="#">Thing 2</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Item 2</a>
+            <ul class="menu vertical sublevel-1">
+                <li><a class="subitem" href="#">Thing 1</a></li>
+                <li><a class="subitem" href="#">Thing 2</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Item 3</a>
+            <ul class="menu vertical sublevel-1">
+                <li><a class="subitem" href="#">Thing 1</a></li>
+                <li><a class="subitem" href="#">Thing 2</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Item 4</a>
+            <ul class="menu vertical sublevel-1">
+                <li>
+                    <a href="#">Sub-item 3</a>
+                    <ul class="menu vertical sublevel-2">
+                        <li><a class="subitem" href="#">Thing 1</a></li>
+                        <li><a class="subitem" href="#">Thing 2</a></li>
+                    </ul>
+                </li>
+                <li><a class="subitem" href="#">Thing 1</a></li>
+                <li><a class="subitem" href="#">Thing 2</a></li>
+            </ul>
+        </li>
+    </ul>
+
+
+
+    <div class="cell small-4 flex-container flex-dir-column">
       <div class="callout primary flex-child-auto"><a href="veiling.php">Veiling</a></div>
       <div class="callout primary flex-child-auto"><a href="veiling.php">Veiling</a></div>
       <div class="callout primary flex-child-auto"><a href="veiling.php">Veiling</a></div>
