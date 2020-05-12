d6d81a07874ba8d710c90154d32f93ef52922ec6
Yoran <yorankoppes@icloud.com>
2020-05-12 13:35:01 +0200

    Front end taalgebruik aangepast

--- veilingCategorieOverzicht.php
+++ veilingCategorieOverzicht.php
@@ -8,49 +8,49 @@ require_once("includes/header.php");
 <div class="grid-x grid-padding-y grid-padding-x">
     <ul class="multilevel-accordion-menu vertical menu" data-accordion-menu>
         <li>
-            <a href="#">Item 1</a>
+            <a href="#">Rubriek 1</a>
             <ul class="menu vertical sublevel-1">
                 <li>
-                    <a href="#">Sub-item 1</a>
+                    <a href="#">Subrubriek 1.1</a>
                     <ul class="menu vertical sublevel-2">
-                        <li><a class="subitem" href="#">Thing 1</a></li>
-                        <li><a class="subitem" href="#">Thing 2</a></li>
-                        <li><a class="subitem" href="#">Thing 3</a></li>
+                        <li><a class="subitem" href="#">Subrubriek 1.1.1</a></li>
+                        <li><a class="subitem" href="#">Subrubriek 1.1.2</a></li>
+                        <li><a class="subitem" href="#">Subrubriek 1.1.3</a></li>
                     </ul>
                 </li>
                 <li>
-                    <a href="#">Sub-item 2</a>
+                    <a href="#">Subrubriek 1.2</a>
                     <ul class="menu vertical sublevel-2">
                         <li>
-                            <a href="#">Super-sub-item 1</a>
+                            <a href="#">Subrubriek 1.2.1</a>
                             <ul class="menu vertical sublevel-3">
-                                <li><a class="subitem" href="#">Thing 1</a></li>
-                                <li><a class="subitem" href="#">Thing 2</a></li>
+                                <li><a class="subitem" href="#">Subrubriek 1.2.1.1</a></li>
+                                <li><a class="subitem" href="#">Subrubriek 1.2.1.2</a></li>
                             </ul>
                         </li>
-                        <li><a class="subitem" href="#">Thing 2</a></li>
+                        <li><a class="subitem" href="#">Subrubriek 1.2.2</a></li>
                     </ul>
                 </li>
-                <li><a class="subitem" href="#">Thing 1</a></li>
-                <li><a class="subitem" href="#">Thing 2</a></li>
+                <li><a class="subitem" href="#">Subrubriek 1.3</a></li>
+                <li><a class="subitem" href="#">Subrubriek 1.4</a></li>
             </ul>
         </li>
         <li>
-            <a href="#">Item 2</a>
+            <a href="#">Rubriek 2</a>
             <ul class="menu vertical sublevel-1">
                 <li><a class="subitem" href="#">Thing 1</a></li>
                 <li><a class="subitem" href="#">Thing 2</a></li>
             </ul>
         </li>
         <li>
-            <a href="#">Item 3</a>
+            <a href="#">Rubriek 3</a>
             <ul class="menu vertical sublevel-1">
                 <li><a class="subitem" href="#">Thing 1</a></li>
                 <li><a class="subitem" href="#">Thing 2</a></li>
             </ul>
         </li>
         <li>
-            <a href="#">Item 4</a>
+            <a href="#">Rubriek 4</a>
             <ul class="menu vertical sublevel-1">
                 <li>
                     <a href="#">Sub-item 3</a>
