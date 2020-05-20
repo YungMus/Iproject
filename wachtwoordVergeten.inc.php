ab362e8c39074640f295a4844ab58002b4e1b52e
Dragon4Ever <eth.ma@student.han.nl>
2020-05-20 13:55:48 +0200

    rubriekenboom js terugbrengen en wachtwoordvergeten stukje bezig

--- wachtwoordVergeten.inc.php
+++ wachtwoordVergeten.inc.php
@@ -1,7 +1,87 @@
 <?php
-if (isset($_POST['send'])) {
-    $sql = "select replace(:oldPassword, :oldPassword, :newPassword) from user WHERE e-mail = :email";
-//    $data = $dbh->prepare($sql);
-//    $data->execute(array( ':oldPassword' => '%'.$oldPassword.'%', ':newPassword' => '%'.$newPassword.'%', ':email' => $email));
+if (isset($_POST['changepassword'])) {
+
+    require 'connectingDatabase.php';
+
+
+    $email = trim(htmlspecialchars($_POST['Email']));
+    $date = date('d/m/Y/G:i:s');
+    $token = md5(time() . $email);
+
+    if (empty($email)) {
+        header("Location: wachtwoordVergeten.php?error=emptyfields");
+        exit();
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: wachtwoordVergeten.php?error=emailinvalid");
+    }
+
+    else if (!checkEmailExists($email, $conn)) {
+        $sql = 'INSERT INTO Password_lost_token (user_id, token_date, token) VALUES (:userID, :token_date, :token)';
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindparam(':userID', $email);
+        $stmt->bindparam(':token_date', $date);
+        $stmt->bindparam(':token', $token);
+        $stmt->execute();
+
+        if ($sql) {
+            $to = $email;
+            $subject = "Email Verificatie";
+            $htmlStr = "";
+            $htmlStr .= "Hi " . $email . ",<br /><br />";
+
+            $htmlStr .= "Klik hieronder aub op het knop om naar het verifieer pagina te gaan.<br /><br /><br />";
+            $htmlStr .= "<a href='http://localhost/Iproject/registerTweedepagina.php?email=.$email.' target='_blank' style='padding:1em; font-weight:bold; background-color:blue; color:#fff;'>Ga naar het website</a><br /><br /><br />";
+
+            $htmlStr .= "Kopieer hieronder je unieke verificatie code.<br /><br /><br />";
+            $htmlStr .= "<p>$token</p><br /><br /><br />";
+
+            $htmlStr .= "Met vriendelijke groeten,<br />";
+            $htmlStr .= "<a href='https://iproject43.icasites.nl/' target='_blank'>EenmaalAndermaal</a><br />";
+
+
+            $name = "EenmaalAndermaal";
+            $email_sender = "no-reply@eenmaalandermaal.com";
+
+            $headers  = "MIME-Version: 1.0\r\n";
+            $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+            $headers .= "From: {$name} <{$email_sender}> \n";
+
+            $body = $htmlStr;
+
+            if (mail($to, $subject, $body, $headers)) {
+                echo("
+                  Message successfully sent!   
+               ");
+                header('Location: registerVoorpagina.php?succes=mailsent');
+            } else {
+                echo("
+                  Message delivery failed...
+               ");
+                header('Location: registerVoorpagina.php?error=mailnotsent');
+            }
+
+        }
+    }
 }
-?>
+
+
+function checkEmailExists($email_to_check, $conn) {
+    $sql = 'SELECT [e-mail]  FROM Email_verification_token WHERE [e-mail]=:email';
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam('email', $email_to_check);
+    $stmt->execute();
+    $stmt = $stmt->fetchAll(PDO::FETCH_NUM);
+    $resultcheck = count($stmt);
+    if ($resultcheck > 0) {
+        header("Location: registerVoorpagina.php?error=emailalreadyused&Email=" . $email_to_check);
+        exit();
+    }
+    if($stmt) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+}
--- wachtwoordVergeten.php
+++ wachtwoordVergeten.php
@@ -2,30 +2,17 @@
 $title = 'Wachtwoord vergeten';
 $link = 'wachtwoordVergeten.php';
 session_start();
-if (($_SERVER['REQUEST_METHOD'] === 'POST')) {
-    if (isset($_POST['send'])){
-        $oldPassword = htmlspecialchars(trim($_POST['oldPassword']));
-        $newPassword = htmlspecialchars(trim($_POST['newPassword']));
-        $email = htmlspecialchars(trim($_POST['email']));
-        print_r($_POST);
-    }
-}
+
 require_once("includes/header.php");
 ?>
 
     <form>
         <div class="form">
             <h4 class="text-center">Wachtwoord veranderen</h4>
-            <label for="form-email">Gebruikersnaam
-                <input type="text" class="form-username" name="email" id="email" >
+            <label for="form-email">Email
+                <input type="text" class="form-email" name="email" id="email" >
             </label>
-            <label for="form-password">Oude wachtwoord
-                <input type="text" class="form-password" name="password" id="password">
-            </label>
-            <label for="form-password">Nieuwe wachtwoord
-                <input type="text" class="form-password" name="password" id="password">
-            </label>
-            <button type="submit" class="form-button">Verander wachtwoord</button>
+            <button type="submit" class="form-button" name="changepassword">Verander wachtwoord</button>
             <p class="text-center"><a href="inlog.php">Toch inloggen?</a></p>
         </div>
     </form>
