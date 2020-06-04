159897a970250847e658d360bf7a147cc44de0c3
YungMus <64072245+YungMus@users.noreply.github.com>
2020-06-04 14:24:35 +0200

    Veiling over datum pagina voor admin

--- Veiling over datum.php
+++ Veiling over datum.php
@@ -4,14 +4,17 @@ $link = 'veilingCategorieOverzicht.php';
 session_start();
 require_once("includes/header.php");
 
-        $sql = "SELECT DISTINCT title, startvalue, item_id FROM Item WHERE running_endday <= (SELECT CONVERT(date, getdate()))";
-        $data = $conn->query($sql);
-        $result = $data->fetchAll();
-        $count = $data->rowCount();
-$html = "";
+if ($_SESSION['Rank'] == " Admin ") {
+    $sql = "SELECT DISTINCT title, startvalue, item_id FROM Item WHERE running_endday <= (SELECT CONVERT(date, getdate())) AND running_endday != '2020-05-28'";
+    $data = $conn->query($sql);
+    $result = $data->fetchAll();
+    $count = $data->rowCount();
+    $html = "";
     for ($i = 0; $i < $count; $i++) {
         $html .= '<div class="callout primary flex-child-auto"><a href="veiling.php?item=' . $result[$i]['item_id'] . '">' . $result[$i]['title'] . ' &euro;' . $result[$i]['startvalue'] . '</a></div>';
     }
+}
+
 echo $html;
 require_once("includes/foundation_script.php");
 require_once("includes/footer.html");
